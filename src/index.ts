f544921ddab4c7e5ae9b3fc1eeba9b9d610bc123
Jeremy H <139379347+jeremygapps@users.noreply.github.com>
2025-03-31 19:59:28 +0000

    Fixed data.js properties, index.ts Data properties

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,14 @@
 //Don't remove this
 interface Data {
-    ID: string[];
-    storeName: string[];
-    type: string[];
-    cost: number[];
-    review: number[];
-    x: number[];
-    y: number[];
+    localTime: string[];
+    pokemonId: string[];
+    longitude: number[];
+    latitude: number[];
   }
   
-  const data: Data = loadJSON("../DO_NOT_TOUCH/data.json") as Data; //Don't delete this line. All your data is here. It does take a few seconds for Replit to load the data because it's so large.
+  const data: Data = loadJSON("../DO_NOT_TOUCH/data.json") as Data; //Don't delete this line. All your data is here. 
   
   //This below just shows you how to access the data. You can remove these.
   console.log(data);
-  console.log(data.ID);
+  console.log(data.pokemonId);
   
