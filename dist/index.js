13246a46a1563a301a688b0133a1a9e4b44c41d1
Jeremy H <139379347+jeremygapps@users.noreply.github.com>
2025-03-31 07:45:16 +0000

    Cleaned data

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const data = loadJSON("../DO_NOT_TOUCH/data.json"); //Don't delete this line. All your data is here. It does take a few seconds for Replit to load the data because it's so large.
+//This below just shows you how to access the data. You can remove these.
+console.log(data);
+console.log(data.ID);
+//# sourceMappingURL=index.js.map
